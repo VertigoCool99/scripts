41568be07139d67aed7b3cf3a22163ade172a3a1
VertigoCool99 <83370667+VertigoCool99@users.noreply.github.com>
2025-07-22 22:46:17 +0100

    Update DrillBitAndCo.lua

--- DrillBitAndCo.lua
+++ DrillBitAndCo.lua
@@ -109,6 +109,187 @@ function LoadMyPlot(PlotName)
         PlaceItem(v.Id,v.CellData,v.Rotation)
     end
 end
+--Locals
+local cloneref = cloneref or function() return end
+local Players = cloneref(game:GetService("Players"))
+local Character = Players.LocalPlayer.Character
+local ClientBuildings = workspace.ClientBuildings
+local Ore = workspace.Ore
+local PlotMod = require(game:GetService("ReplicatedStorage").Plot.PlotClient)
+local HttpService = game:GetService("HttpService")
+local GameFunsMod = require(game:GetService("ReplicatedStorage").Tool.Game.GameFunctions)
+local UiSrcMod = require(game:GetService("ReplicatedStorage").UISrc.UIModules.PlotActions)
+local AutoLoadedPlot = false
+
+--Tables
+local Settings = {AutoCrates=false,AutoAllUpgraders=false,AutoOpenCrates=false,AutoSelectedUpgrader=false,SelectedUpgrader="",AutoRebirth=false,PlotName="",AutoPlotUpgrade=false,AutoLoadLayout=false}
+local Boxes = {["Regular Box"]=1,["Unreal Box"]=2,["Rebirth Box"]=3}
+local PlotList = {}
+
+--Init
+assert(listfiles,"Executor Not Supported | Missing listfiles")
+assert(readfile,"Executor Not Supported | Missing readfile")
+assert(writefile,"Executor Not Supported | Missing writefile")
+assert(getupvalues,"Executor Not Supported | Missing getupvalues")
+assert(getupvalue,"Executor Not Supported | Missing getupvalue")
+
+
+for i,v in pairs(listfiles("DrillBitAndCo")) do
+    if isfile(v) then 
+        local old = string.split(v,"DrillBitAndCo")[2]
+        table.insert(PlotList,string.sub(old,2,string.len(old)-5)) 
+    end
+end
+
+
+--Functions
+local PlotPrices = getupvalues(UiSrcMod.Setup)[3].plotSize
+
+function GetMyPlot() 
+	return ClientBuildings:FindFirstChild(Players.LocalPlayer.Name)
+end
+
+function GetMyOres() 
+	return Ore:FindFirstChild(Players.LocalPlayer.Name):GetChildren()
+end
+
+function GetMyUpgraders()
+	local tbl = {}
+	for i,v in pairs(GetMyPlot():GetChildren()) do
+		if v:FindFirstChild("UpgradePart") then
+			tbl[v.Name] = v.UpgradePart
+		end
+	end
+	return tbl
+end
+
+function ItemLookup(Name)
+    assert(Name,"No name provided [ItemLookup]")
+    for i,v in pairs(game:GetService("ReplicatedStorage").Collection.Item:GetChildren()) do
+        for i2,v2 in pairs(v:GetChildren()) do
+            if v2:IsA("ModuleScript") and Name == v2.Name then
+                return v2,v2:GetAttribute("id")
+            end
+        end
+    end
+    return "",0
+end
+function ItemLookupId(Id)
+    assert(Id,"No id provided [ItemLookup]")
+    for i,v in pairs(game:GetService("ReplicatedStorage").Collection.Item:GetChildren()) do
+        for i2,v2 in pairs(v:GetChildren()) do
+            if v2:IsA("ModuleScript") and Id == v2:GetAttribute("id") then
+                return v2,v2:GetAttribute("id")
+            end
+        end
+    end
+    return "",0
+end
+
+function GetSelectedUpgrader(UpgraderName)
+    local Upgrader,UpgraderId = ItemLookup(UpgraderName)
+    if not Upgrader.Name == UpgraderName then return end
+    for i,v in pairs(GetMyPlot():GetChildren()) do
+        if v:GetAttribute("itemId") == UpgraderId then
+            return v:FindFirstChild("UpgradePart")
+        end
+    end
+end
+
+function PlaceItem(Id,CellData,Rotation)
+    local BuildId = getupvalue(PlotMod.PlaceItems,16)
+    game:GetService("ReplicatedStorage").Plot.Remote.RequestPlace:InvokeServer(BuildId+1,{id=Id,frontLeftCell=CellData,rotation=Rotation})
+    setupvalue(PlotMod.PlaceItems,16,BuildId+1)
+end
+
+function SaveMyPlot(PlotName)
+    local tbl = {}
+    for i,v in pairs(GetMyPlot():GetChildren()) do
+        if v:IsA("Model") then
+            table.insert(tbl,{Id=v:GetAttribute("itemId"),CellData=v:GetAttribute("frontLeftCell"),Rotation=v:GetAttribute("rotation")})
+        end
+    end
+    local PlotEncoded = HttpService:JSONEncode(tbl)
+    writefile("DrillBitAndCo/"..PlotName..".json",PlotEncoded)
+end
+function LoadMyPlot(PlotName)
+    if not isfile("DrillBitAndCo/"..PlotName..".json") then return end
+    local Decoded = HttpService:JSONDecode(readfile("DrillBitAndCo/"..PlotName..".json"))
+
+    for i,v in pairs(Decoded) do
+        PlaceItem(v.Id,v.CellData,v.Rotation)
+    end
+end
+
+function CategorizeUpgraders()
+	local withRange = {}
+	local noRange = {}
+	local special = {}
+	local oreWash = nil
+	for _, model in pairs(GetMyPlot():GetChildren()) do
+		if model:IsA("Model") and model:FindFirstChild("UpgradePart") and model:GetAttribute("itemId") then
+			local Module, _ = ItemLookupId(model:GetAttribute("itemId"))
+			local upgradePart = model.UpgradePart
+			if Module then
+				module = require(Module)
+				local isSpecial = rawget(module, "upgradeFunction")
+				local range = rawget(module, "upgradeRange")
+
+				if string.find(Module.Name:lower(), "wash") then
+					oreWash = upgradePart
+				elseif isSpecial then
+					table.insert(special, {Part = upgradePart, Model = model})
+				elseif range then
+					table.insert(withRange, {
+						Part = upgradePart,
+						Model = model,
+						Min = range[1],
+						Max = range[2]
+					})
+				else
+					table.insert(noRange, {Part = upgradePart, Model = model})
+				end
+			end
+		end
+	end
+	table.sort(withRange, function(a, b)
+		return a.Max > b.Max
+	end)
+
+	return withRange, noRange, special, oreWash
+end
+
+function RunSmartUpgraderLoop()
+	local withRange, noRange, special, oreWash = CategorizeUpgraders()
+	while Settings.AutoAllUpgraders == true do
+		task.wait()
+		for _, ore in pairs(GetMyOres()) do
+			local value = ore:GetAttribute("Value") or 0
+			local upgraded = false
+			for _, upgrader in pairs(withRange) do
+				if value >= upgrader.Min and value <= upgrader.Max then
+					firetouchinterest(upgrader.Part, ore, 0)
+					firetouchinterest(upgrader.Part, ore, 1)
+					upgraded = true
+				end
+			end
+			if not upgraded then
+				for _, upgrader in pairs(noRange) do
+					firetouchinterest(upgrader.Part, ore, 0)
+					firetouchinterest(upgrader.Part, ore, 1)
+				end
+			end
+			for _, upgrader in pairs(special) do
+				firetouchinterest(upgrader.Part, ore, 0)
+				firetouchinterest(upgrader.Part, ore, 1)
+                if oreWash then
+                    firetouchinterest(oreWash, ore, 0)
+                    firetouchinterest(oreWash, ore, 1)
+                end
+			end
+		end
+	end
+end
 
 local repo = 'https://raw.githubusercontent.com/violin-suzutsuki/LinoriaLib/main/'
 
@@ -156,17 +337,7 @@ OresGroupBox:AddToggle('AutoOres', {
     Callback = function(Value)
         Settings.AutoAllUpgraders = Value
         task.spawn(function()
-            local Upgraders = GetMyUpgraders()
-            while Settings.AutoAllUpgraders == true do task.wait()
-                for i,v in pairs(GetMyOres()) do
-                    for i2,v2 in pairs(Upgraders) do
-                        if v ~= nil and v2 ~= nil then
-                            firetouchinterest(v2, v, 0)
-                            firetouchinterest(v2, v, 1)
-                        end
-                    end
-                end
-            end
+            RunSmartUpgraderLoop()
         end)
     end
 })
@@ -250,8 +421,7 @@ RebirthGroupBox:AddToggle('AutoPlotUpgrade', {
         Settings.AutoPlotUpgrade = value
         task.spawn(function()
             while Settings.AutoPlotUpgrade == true do task.wait(.2)
-                local tier = Players.LocalPlayer:GetAttribute("plotSize")
-                if tier == 10 then return end
+                if Players.LocalPlayer:GetAttribute("plotSize") == 10 then return end
                 if Players.LocalPlayer:GetAttribute("cash") >= PlotPrices[Players.LocalPlayer:GetAttribute("plotSize")+1] then
                     game:GetService("ReplicatedStorage"):WaitForChild("Remote"):WaitForChild("PurchaseMasteryUpgrade"):InvokeServer("plotSize")
                 end
