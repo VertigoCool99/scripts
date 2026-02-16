00e58b7837b34afaaf8564c7a30eaf4978143a67
VertigoCool99 <83370667+VertigoCool99@users.noreply.github.com>
2026-02-16 17:45:26 +0000

    Update and rename Reincarnatede.lua to Reincarnated.lua

--- Reincarnated.lua
+++ Reincarnated.lua
@@ -532,7 +532,7 @@ ThemeManager:ApplyToTab(Tabs['UI Settings'])
 SaveManager:LoadAutoloadConfig()
 
 if queue_on_teleport ~= nil then
-    queue_on_teleport('loadstring(game:HttpGet("https://raw.githubusercontent.com/VertigoCool99/scripts/refs/heads/main/Reincarnatede.lua"))()')
+    queue_on_teleport('loadstring(game:HttpGet("https://raw.githubusercontent.com/VertigoCool99/scripts/refs/heads/main/Reincarnated.lua"))()')
 end
 
 local file = "Reincarnated_Hopped.txt"
