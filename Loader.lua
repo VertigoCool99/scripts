92c358cdb3fa390e88081cd97ac25c7007fd2a42
VertigoCool99 <83370667+VertigoCool99@users.noreply.github.com>
2026-04-13 19:11:43 +0100

    Add Loader.lua to dynamically load game scripts

---
+++ Loader.lua
@@ -0,0 +1,5 @@
+local Games = {
+    [131603408057162] = "CageFishing"
+}
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/VertigoCool99/scripts/refs/heads/main/".. Games[game.PlaceId] ..".lua"))()
